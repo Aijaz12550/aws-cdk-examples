4cd0e14b11d04e3eeace61d389f1bab60112ab4f
Aijaz Abbasi <muhammadaijaz76@gmail.com>
2021-04-11 20:07:47 +0500

    rest api with lambda

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
-
+import * as apigw from "@aws-cdk/aws-apigateway";
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -11,6 +11,31 @@ export class CdkStack extends cdk.Stack {
       code: lambda.Code.fromAsset("src/lambda-functions/hello-world"),
       handler: "index.helloWorld",
       runtime: lambda.Runtime.NODEJS_12_X,
+      functionName:"helloWorld"
     });
+
+    const helloLambda1 = new lambda.Function(this, "helloLambda1", {
+      code: lambda.Code.fromAsset("src/lambda-functions/hello-world"),
+      handler: "index.helloWorld",
+      runtime: lambda.Runtime.NODEJS_12_X,
+      functionName:"helloWorld1"
+    });
+
+    // API GATEWAY **********
+    let api = new apigw.RestApi(this, "hello api",{
+      restApiName:"example api"
+    })
+
+    // baseUrl/test to invoke helloLambda
+    let item = api.root.addResource("test");
+    let helloLambdaTrigger = new apigw.LambdaIntegration(helloLambda);
+    item.addMethod("Get",helloLambdaTrigger)
+
+    // baseUrl/test to invoke helloLambda1
+    let item2 = api.root.addResource("test2");
+    let api1 = new apigw.LambdaIntegration(helloLambda1)
+    item2.addMethod("GET",api1)
+    
+
   }
 }
--- src/lambda-functions/hello-world/index.ts
+++ src/lambda-functions/hello-world/index.ts
@@ -1,7 +1,9 @@
 
-export const helloWorld = () => {
+export const helloWorld = async() => {
     console.log("Hello World !!")
     return {
+        statusCode:200,
+        headers:{"content-type":"text/plain"},
         body: "Hello"
     }
 }
